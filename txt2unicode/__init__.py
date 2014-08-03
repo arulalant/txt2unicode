180b19638e480482b33b2ab451b45cd4570bc105
arulalant <arulalant@gmail.com>
2014-08-03 22:36:16 +0530

    encode2unicde convertor

---
+++ txt2unicode/__init__.py
@@ -0,0 +1,4 @@
+from encode2unicode import anjal2unicode, bamini2unicode, boomi2unicode, \
+    dinakaran2unicode, dinamani2unicode, dinathanthy2unicode, \
+    kavipriya2unicode, murasoli2unicode, mylai2unicode, nakkeeran2unicode, \
+    roman2unicode, tab2unicode, tam2unicode, tscii2unicode
